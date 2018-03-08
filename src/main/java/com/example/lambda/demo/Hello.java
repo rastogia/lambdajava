a9b4b8ae8e3f6dc3ad1c3685ef1d29f4978085ca
rastogia <akash.rastogi@hays.com>
2018-03-08 15:14:26 +0100

    Add files via upload

---
+++ src/main/java/com/example/lambda/demo/Hello.java
@@ -0,0 +1,16 @@
+package com.example.lambda.demo;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class Hello implements RequestHandler<Object, String> {
+
+	public String handleRequest(Object input, Context context) {
+	  context.getLogger().log("Input: " + input);
+	  String output = "Hello, " + input + "!";
+	  return output;
+	}
+
+	
+
+}
