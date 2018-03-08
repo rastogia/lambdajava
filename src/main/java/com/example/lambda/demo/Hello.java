680015531f39f61e03e2b5bd93351dab700da019
rastogia <akash.rastogi@hays.com>
2018-03-08 15:58:52 +0100

    Update Hello.java

--- src/main/java/com/example/lambda/demo/Hello.java
+++ src/main/java/com/example/lambda/demo/Hello.java
@@ -8,6 +8,7 @@ public class Hello implements RequestHandler<Object, String> {
 	public String handleRequest(Object input, Context context) {
 	  context.getLogger().log("Input: " + input);
 	  String output = "Hello, good day " + input + "!";
+	  String output = "Hello, good day " + input "!"; //fail build
 	  return output;
 	}
 
